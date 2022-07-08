351fe374712de0b8b5521c81bd63d45e164fc4a8
siha <siha@siha-MacBookPro.local>
2022-07-08 18:12:53 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function about() {
+  document.querySelector("#info-content").innerHTML =
+    "KoreaUniv Computer Science and Engineering</br>Front-End Developer<br />HTML / CSS / JS<br />";
+}
+
+function like() {
+  document.querySelector("#info-content").innerHTML =
+    "Piano<br />Weight Training<br />Netflix";
+}
+
+function elice() {
+  document.querySelector("#info-content").innerHTML =
+    "Team Project<br />Web Skill<br />Algorithm Skill";
+}
